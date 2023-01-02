09afc57eb3f28b461a1ba311d4a8ab1a8bca428d
Artur <artur.augustyn26@gmail.com>
2023-01-02 16:17:17 +0100

    added command builder and  main  function

---
+++ src/CommandBuilder.java
@@ -0,0 +1,40 @@
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class CommandBuilder {
+    Optional<Command> buildCommand(final String line) {
+        String[] split = line.split(";");
+        String commandType = split[0];
+        if (!Command.Type.valuesAsList().contains(commandType)) {
+            System.err.printf("User provided unknown command: [%s]%n", commandType);
+            return Optional.empty();
+        }
+        var parametersMap = Arrays.asList(split).subList(1, split.length).stream()
+                .map(item -> item.split("="))
+                .collect(Collectors.toMap(itemSplit -> itemSplit[0], itemSplit -> itemSplit[1]));
+
+        ToDoItem toDoItem = buildToDoItem(parametersMap);
+
+        return Optional.of(new Command(Command.Type.from(commandType), toDoItem));
+    }
+
+    private ToDoItem buildToDoItem(final Map<String, String> parametersMap) {
+        ToDoItem toDoItem = new ToDoItem();
+        Optional.ofNullable(parametersMap.get(ToDoItem.Field.NAME.name()))
+                .ifPresent(toDoItem::setName);
+
+        Optional.ofNullable(parametersMap.get(ToDoItem.Field.DESCRIPTION.name()))
+                .ifPresent(toDoItem::setDescription);
+
+        Optional.ofNullable(parametersMap.get(ToDoItem.Field.DEADLINE.name()))
+                .ifPresent(deadline -> toDoItem.setDeadLine(LocalDateTime.parse(deadline, ToDoItem.DATE_FORMAT)));
+
+        Optional.ofNullable(parametersMap.get(ToDoItem.Field.PRIORITY.name()))
+                .ifPresent(priority -> toDoItem.setPriority(Integer.valueOf(priority)));
+        return toDoItem;
+
+    }
+}
---
+++ src/ToDoItem.java
@@ -0,0 +1,60 @@
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ToDoItem {
+    public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+    private String name;
+    private String description;
+    private LocalDateTime deadLine;
+    private Integer priority;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(LocalDateTime deadLine) {
+        this.deadLine = deadLine;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDoItem{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", deadLine=" + deadLine +
+                ", priority=" + priority +
+                '}';
+    }
+
+    public enum Field {
+        NAME,
+        DESCRIPTION,
+        DEADLINE,
+        PRIORITY
+    }
+}
